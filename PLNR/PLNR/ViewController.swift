a753ea6802ba249e6eb6b4e7bdf77bca55296117
William Reed <wreed2020@fau.edu>
2023-11-04 18:48:53 -0400

    Initialize the app!

---
+++ PLNR/PLNR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PLNR
+//
+//  Created by Will Reed on 11/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
