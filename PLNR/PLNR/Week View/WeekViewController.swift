bea3b3917c2c49ebffc795a66554b179ce077f06
William Reed <wreed2020@fau.edu>
2023-11-19 17:41:02 -0500

    open to week view upon login

--- PLNR/PLNR/Week View/WeekViewController.swift
+++ PLNR/PLNR/Week View/WeekViewController.swift
@@ -1,13 +1,13 @@
 //
-//  DayViewController.swift
+//  WeekViewController.swift
 //  PLNR
 //
-//  Created by Will Reed on 11/7/23.
+//  Created by Will Reed on 11/19/23.
 //
 
 import UIKit
 
-class DayViewController: UIViewController {
+class WeekViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,9 +15,9 @@ class DayViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    /*@IBAction func onLogoutTapped(_ sender: Any) {
+    @IBAction func onLogoutTapped(_ sender: Any) {
         showConfirmLogoutAlert()
-    }*/
+    }
     
     private func showConfirmLogoutAlert() {
         let alertController = UIAlertController(title: "Log out of \(User.current?.username ?? "current account")?", message: nil, preferredStyle: .alert)
@@ -30,15 +30,3 @@ class DayViewController: UIViewController {
         present(alertController, animated: true)
     }
 }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
